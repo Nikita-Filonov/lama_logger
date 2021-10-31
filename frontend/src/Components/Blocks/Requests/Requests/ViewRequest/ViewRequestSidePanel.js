e739936d7d5646a538a65903ac5279390188fa86
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2021-11-01 00:03:43 +0300

    Update ViewRequestSidePanel.js

--- frontend/src/Components/Blocks/Requests/Requests/ViewRequest/ViewRequestSidePanel.js
+++ frontend/src/Components/Blocks/Requests/Requests/ViewRequest/ViewRequestSidePanel.js
@@ -1,13 +1,34 @@
-import React from "react";
-import {Paper} from "@mui/material";
+import React, {memo} from "react";
+import {Divider, IconButton, Paper, Tooltip} from "@mui/material";
 import ViewRequestAccordion from "./ViewRequestAccordion";
+import ViewRequestMenu from "../../../../Menus/Requests/Requests/ViewRequestMenu";
+import {Close} from "@mui/icons-material";
+import {connect} from "react-redux";
+import {setRequest} from "../../../../../Redux/Requests/Requests/requestsActions";
 
-export const ViewRequestSidePanel = ({request}) => {
+const ViewRequestSidePanel = ({request, setRequest}) => {
+  const onClose = () => setRequest({});
+
   return (
     <Paper sx={{ml: 2}}>
+      <div className={'d-flex align-items-center p-2'}>
+        <Tooltip title={'Close request'}>
+          <IconButton size={'small'} onClick={onClose}>
+            <Close/>
+          </IconButton>
+        </Tooltip>
+        <div className={'flex-grow-1'}/>
+        <ViewRequestMenu request={request}/>
+      </div>
+      <Divider/>
       <ViewRequestAccordion request={request}/>
     </Paper>
   )
 }
 
-
+export default connect(
+  null,
+  {
+    setRequest,
+  },
+)(memo(ViewRequestSidePanel));
