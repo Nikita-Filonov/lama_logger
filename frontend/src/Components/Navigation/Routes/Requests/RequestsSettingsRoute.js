47ca3ac59978bf27bfbb624745bce3d7d3a2c6a0
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2021-11-02 23:02:08 +0300

    Update RequestsSettingsRoute.js

--- frontend/src/Components/Navigation/Routes/Requests/RequestsSettingsRoute.js
+++ frontend/src/Components/Navigation/Routes/Requests/RequestsSettingsRoute.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {matchPath, Redirect, Route, useLocation} from 'react-router-dom'
 import {Container} from "@mui/material";
 import ProjectSettingsSidebar from "../../../Blocks/Requests/Settings/ProjectSettingsSidebar";
 import NavigationBar from "../../NavigationBar";
 import RequestNavigationDrawer from "../../Drawers/Requests/RequestNavigationDrawer";
-import {RequestRoute} from "./RequestRoute";
+import {useProjects} from "../../../../Providers/ProjectsProvider";
+import {REQUESTS_ROUTES} from "../../../../Utils/Constants";
+import {useSelector} from "react-redux";
 
 export const RequestsSettingsRoute = ({component: Component, ...rest}) => {
+  const token = localStorage.getItem('token');
+  const {getProject} = useProjects();
+  const {pathname} = useLocation();
+
+  const storeProject = useSelector(state => state.projects.project);
+
+  useEffect(() => {
+    (async () => {
+      const props = matchPath(pathname, {path: REQUESTS_ROUTES});
+      const projectId = parseInt(props?.params?.projectId);
+      ((projectId !== storeProject?.id) && token) && await getProject(projectId);
+    })()
+  }, [pathname])
 
   return (
     <React.Fragment>
@@ -13,7 +29,16 @@ export const RequestsSettingsRoute = ({component: Component, ...rest}) => {
       <Container maxWidth={'xl'}>
         <ProjectSettingsSidebar/>
         <div>
-          <RequestRoute component={Component} {...rest}/>
+          <Route
+            {...rest}
+            render={props =>
+              token ? (
+                <Component {...props} />
+              ) : (
+                <Redirect to={{pathname: '/login', state: {from: props.location}}}/>
+              )
+            }
+          />
         </div>
       </Container>
     </React.Fragment>
