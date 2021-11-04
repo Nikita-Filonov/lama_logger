4dd6e18b78cc1169cd8b184a956364a8856f1350
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2021-11-04 12:47:05 +0300

    Добавлено обновление и удаление доменов

--- frontend/src/Components/Items/Reuqests/Settings/Tracks/Domain.js
+++ frontend/src/Components/Items/Reuqests/Settings/Tracks/Domain.js
@@ -1,18 +1,39 @@
 import React from "react";
-import {ListItem, ListItemIcon, ListItemSecondaryAction, ListItemText} from "@mui/material";
-import {Language} from "@mui/icons-material";
-import DomainMenu from "../../../../Menus/Requests/Settings/Tracks/DomainMenu";
+import {Grid, IconButton, TextField} from "@mui/material";
+import {Close, Language} from "@mui/icons-material";
 
-export const Domain = ({domain}) =>
-  <ListItem divider>
-    <ListItemIcon>
-      <Language/>
-    </ListItemIcon>
-    <ListItemText
-      primary={'sdfsfdsfsfsd'}
-      secondary={'http://localhost:8000/'}
-    />
-    <ListItemSecondaryAction>
-      <DomainMenu/>
-    </ListItemSecondaryAction>
-  </ListItem>
+export const Domain = ({domain, index, onChange, onRemove}) =>
+  <Grid container xs={12} spacing={2}>
+    <Grid item xs={4}>
+      <div className={'d-flex justify-content-center align-items-end mt-2'}>
+        <Language sx={{color: 'action.active', mr: 2, my: 0.5}}/>
+        <TextField
+          value={domain?.name}
+          onChange={async event => await onChange('name', index, event.target.value)}
+          fullWidth
+          variant={'standard'}
+          size={'small'}
+          placeholder={'{my_pattern}'}
+          label={'Pattern'}
+          className={'me-1'}
+        />
+      </div>
+    </Grid>
+    <Grid item xs={8}>
+      <div className={'d-flex justify-content-center align-items-center mt-2'}>
+        <TextField
+          value={domain?.domain}
+          onChange={async event => await onChange('domain', index, event.target.value)}
+          fullWidth
+          variant={'standard'}
+          size={'small'}
+          placeholder={'^\\d$'}
+          label={'Regex expression'}
+          className={'me-2'}
+        />
+        <IconButton size={'small'} sx={{mt: 2}} onClick={async () => await onRemove(index)}>
+          <Close fontSize={'small'}/>
+        </IconButton>
+      </div>
+    </Grid>
+  </Grid>
--- frontend/src/Screens/Requests/Settings/Tracks/TracksDomainsSettings.js
+++ frontend/src/Screens/Requests/Settings/Tracks/TracksDomainsSettings.js
@@ -1,20 +1,36 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import {ProjectSettingsStyles} from "../../../../Styles/Screens";
 import {connect} from "react-redux";
 import {ProjectSettingsHeader} from "../../../../Components/Blocks/Requests/Settings/ProjectSettingsHeader";
-import {List, Typography} from "@mui/material";
+import {Grid, List, Typography} from "@mui/material";
 import {ZoomFab} from "../../../../Components/Blocks/Common/ZoomFab";
 import {Domain} from "../../../../Components/Items/Reuqests/Settings/Tracks/Domain";
 import CreateTrackDomain from "../../../../Components/Modals/Requests/Settings/Tracks/CreateTrackDomain";
+import Box from "@mui/material/Box";
+import {LoadingButton} from "@mui/lab";
+import {SaveOutlined} from "@mui/icons-material";
+import {useProjectSettings} from "../../../../Providers/Requests/ProjectSettingsProvider";
 
 
-const TracksDomainsSettings = ({projectSettings}) => {
+const TracksDomainsSettings = ({project, projectSettings}) => {
   const classes = ProjectSettingsStyles();
+  const {request, updateProjectSettings} = useProjectSettings();
   const [trackDomains, setTrackDomains] = useState(projectSettings?.trackDomains);
   const [createDomainModal, setCreateDomainModal] = useState(false);
 
+  const disabled = useMemo(() => trackDomains === projectSettings?.trackDomains, [trackDomains])
+
   useEffect(() => setTrackDomains(projectSettings?.trackDomains), [projectSettings?.trackDomains]);
 
+  const onRemove = async (index) => {
+    const copyTrackDomains = [...trackDomains];
+    copyTrackDomains.splice(index, 1);
+    setTrackDomains(copyTrackDomains);
+    await updateProjectSettings(project.id, {trackDomains: copyTrackDomains});
+  }
+  const onChange = async (type = 'pattern', index, value) =>
+    setTrackDomains(trackDomains.map((p, i) => i === index ? {...p, [type]: value} : p));
+  const onSave = async () => await updateProjectSettings(project.id, {trackDomains});
 
   return (
     <div className={classes.contentContainer}>
@@ -27,9 +43,26 @@ const TracksDomainsSettings = ({projectSettings}) => {
           <Domain
             key={index}
             domain={domain}
+            index={index}
+            onChange={onChange}
+            onRemove={onRemove}
           />
         )}
       </List>
+      <Grid item xs={12} className={'mt-3'}>
+        <Box className={'position-relative'}>
+          <LoadingButton
+            loadingPosition="start"
+            startIcon={<SaveOutlined/>}
+            disabled={disabled}
+            variant="text"
+            onClick={onSave}
+            loading={request}
+          >
+            Save changes
+          </LoadingButton>
+        </Box>
+      </Grid>
       <ZoomFab title={'New domain'} action={() => setCreateDomainModal(true)}/>
       <CreateTrackDomain
         modal={createDomainModal}
@@ -42,7 +75,8 @@ const TracksDomainsSettings = ({projectSettings}) => {
 
 
 const getState = (state) => ({
-  projectSettings: state.projects.projectSettings,
+  project: state.projects.project,
+  projectSettings: state.projects.projectSettings
 })
 
 export default connect(
