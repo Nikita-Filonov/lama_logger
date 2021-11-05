c284e0328bb9a4b31bbf9a5a2c01dc2202e9327d
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2021-11-06 01:34:34 +0300

    Update RequestHeaders.js

--- frontend/src/Components/Blocks/Requests/CustomRequests/Request/RequestHeaders.js
+++ frontend/src/Components/Blocks/Requests/CustomRequests/Request/RequestHeaders.js
@@ -14,34 +14,31 @@ const RequestHeaders = ({disabled = false, customRequest, setCustomRequest}) => 
       .map(header => ({key: header, value: customRequest?.requestHeaders[header], include: true}))
   ), [customRequest?.requestHeaders])
 
-  const onChange = useCallback(async (value, index, key) => {
-      const newHeaders = headers.map((payload, i) => i === index ? {...payload, [key]: value} : payload);
-      setHeaders(newHeaders);
-      await updateHeadersPayload(newHeaders);
-    }, [headers]
-  );
-  const onNewHeader = async () => {
+  const onChangeInput = useCallback(async (value, index, key) => {
+    const newHeaders = headers.map((payload, i) => i === index ? {...payload, [key]: value} : payload);
+    setHeaders(newHeaders);
+    await updateHeadersPayload(newHeaders);
+  }, [headers])
+
+  const onNewHeader = useCallback(async () => {
     const newHeaders = [...headers, {key: '', value: '', include: true}];
     setHeaders(newHeaders);
     await updateHeadersPayload(newHeaders);
-  };
-  const onRemove = async (index) => {
+  }, [headers]);
+  const onRemove = useCallback(async (index) => {
     const newHeaders = headers.filter((_, i) => i !== index);
     setHeaders(newHeaders);
     await updateHeadersPayload(newHeaders);
-  }
+  }, [headers])
 
-  const updateHeadersPayload = async (requestHeaders) => {
+  const updateHeadersPayload = useCallback(async (requestHeaders) => {
     let safeHeaders = {};
     for (let i = 0; i < requestHeaders.length; i++) {
-      const key = headers[i].key;
-      const value = headers[i].value;
-      if (key && value) {
-        safeHeaders[key] = value;
-      }
+      const key = requestHeaders[i]?.key;
+      safeHeaders[key] = requestHeaders[i]?.value;
     }
-    setCustomRequest({...customRequest, safeHeaders})
-  };
+    setCustomRequest({...customRequest, requestHeaders: safeHeaders})
+  }, [customRequest?.requestHeaders]);
 
   return (
     <div className={classes.requestHeadersContainer}>
@@ -50,13 +47,13 @@ const RequestHeaders = ({disabled = false, customRequest, setCustomRequest}) => 
           <Checkbox
             size={'small'}
             checked={include}
-            onClick={async event => await onChange(event.target.checked, index, 'include')}
+            onClick={async event => await onChangeInput(event.target.checked, index, 'include')}
           />
           <TextField
             disabled={disabled}
             sx={{mr: 2}}
             value={key}
-            onChange={async event => await onChange(event.target.value, index, 'key')}
+            onChange={async event => await onChangeInput(event.target.value, index, 'key')}
             fullWidth
             variant={'standard'}
             size={'small'}
@@ -65,7 +62,7 @@ const RequestHeaders = ({disabled = false, customRequest, setCustomRequest}) => 
           <TextField
             disabled={disabled}
             value={value}
-            onChange={async event => await onChange(event.target.value, index, 'value')}
+            onChange={async event => await onChangeInput(event.target.value, index, 'value')}
             fullWidth
             variant={'standard'}
             size={'small'}
