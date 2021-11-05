7ef79b710314973e21da6672920bfb7d736630a0
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2021-11-05 22:03:13 +0300

    Update RequestHeaders.js

--- frontend/src/Components/Blocks/Requests/CustomRequests/Request/RequestHeaders.js
+++ frontend/src/Components/Blocks/Requests/CustomRequests/Request/RequestHeaders.js
@@ -1,28 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Checkbox, IconButton, TextField} from "@mui/material";
 import {connect} from "react-redux";
 import {setCustomRequest} from "../../../../../Redux/Requests/CustomRequests/customRequestsActions";
 import {Close} from "@mui/icons-material";
 import {CustomRequestsStyles} from "../../../../../Styles/Screens";
 
-const RequestHeaders = ({customRequest}) => {
+const RequestHeaders = ({disabled = false, customRequest, setCustomRequest}) => {
   const classes = CustomRequestsStyles();
+  const [headers, setHeaders] = useState([]);
+
+  useEffect(() => setHeaders(
+    Object.keys(customRequest?.requestHeaders)
+      .map(header => ({key: header, value: customRequest?.requestHeaders[header], include: true}))
+  ), [customRequest?.requestHeaders])
+
+  const onInclude = async (event, index) =>
+    setHeaders(headers.map((payload, i) => i === index ? {...payload, include: event.target.checked} : payload));
 
   return (
     <div className={classes.requestHeadersContainer}>
-      {Object.keys(customRequest?.requestHeaders)?.map((header, index) =>
+      {headers?.map(({key, value, include}, index) =>
         <div className={'d-flex align-items-center'} key={index}>
-          <Checkbox size={'small'}/>
+          <Checkbox size={'small'} checked={include} onClick={async event => await onInclude(event, index)}/>
           <TextField
+            disabled={disabled}
             sx={{mr: 2}}
-            value={header}
+            value={key}
             fullWidth
             variant={'standard'}
             size={'small'}
             placeholder={'Key'}
           />
           <TextField
-            value={customRequest?.requestHeaders[header]}
+            disabled={disabled}
+            value={value}
             fullWidth
             variant={'standard'}
             size={'small'}
