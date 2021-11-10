af8f62de8b7095aea0ef7002b35598a071fa9387
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2021-11-10 21:11:15 +0300

    Update urls.py

--- lama_logger/urls.py
+++ lama_logger/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework.authtoken import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/' if settings.DEBUG else 'other-admin/', admin.site.urls),
     path('api/v1/', include('core.users.urls')),
     path('api/v1/', include('core.stats.urls')),
     path('api/v1/', include('core.calls.urls')),
