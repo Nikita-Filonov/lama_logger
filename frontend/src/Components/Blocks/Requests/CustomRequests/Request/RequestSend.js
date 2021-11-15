d858b791c41ba6ea788d33a90960b66e8d977bd0
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2021-11-15 20:10:33 +0300

    Update RequestSend.js

--- frontend/src/Components/Blocks/Requests/CustomRequests/Request/RequestSend.js
+++ frontend/src/Components/Blocks/Requests/CustomRequests/Request/RequestSend.js
@@ -1,36 +1,14 @@
 import React from "react";
 import {Send} from "@mui/icons-material";
-import {connect} from "react-redux";
 import {LoadingButton} from "@mui/lab";
-import {useCustomRequests} from "../../../../../Providers/Requests/CustomRequestsPorvider";
 
-const RequestSend = ({project, customRequest}) => {
-  const {request, sendCustomRequest, createCustomRequestsHistory} = useCustomRequests();
-
-  const sendRequest = async () => {
-    await sendCustomRequest(project.id, customRequest.requestId)
-    await createCustomRequestsHistory(project.id, customRequest);
-  }
-
-  return (
-    <LoadingButton
-      sx={{ml: 2, pr: 2, pl: 2}}
-      endIcon={<Send/>}
-      onClick={sendRequest}
-      loading={request}
-      loadingPosition="end"
-    >
-      Send
-    </LoadingButton>
-  )
-}
-
-const getState = (state) => ({
-  project: state.projects.project,
-  customRequest: state.customRequests.customRequest,
-})
-
-export default connect(
-  getState,
-  null,
-)(RequestSend);
+export const RequestSend = ({request, sendRequest}) =>
+  <LoadingButton
+    sx={{ml: 2, pr: 2, pl: 2}}
+    endIcon={<Send/>}
+    onClick={sendRequest}
+    loading={request}
+    loadingPosition="end"
+  >
+    Send
+  </LoadingButton>
