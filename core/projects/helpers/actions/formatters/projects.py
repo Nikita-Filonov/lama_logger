22b9eadf66d94292feb4c5ad50cced9ef38f5a0a
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2021-11-20 01:33:42 +0300

    Трэки актионов для проекта

--- core/projects/helpers/actions/formatters/projects.py
+++ core/projects/helpers/actions/formatters/projects.py
@@ -2,6 +2,7 @@ from typing import Dict
 
 from core.projects.helpers.actions.formatters.utils import default
 from core.projects.localization.en.projects import FIELDS_EN
+from core.users.models import CustomUser
 
 LANGUAGES = {
     'en': FIELDS_EN,
@@ -16,14 +17,36 @@ def project_title(**kwargs) -> Dict[str, str]:
     return default(LANGUAGES, 'title', **kwargs)
 
 
-def project_title(**kwargs) -> Dict[str, str]:
+def project_short(**kwargs) -> Dict[str, str]:
     """
     :param kwargs:
     :return:
     """
-    return default(LANGUAGES, 'title', **kwargs)
+    return default(LANGUAGES, 'short', **kwargs)
+
+
+def project_description(**kwargs) -> Dict[str, str]:
+    """
+    :param kwargs:
+    :return:
+    """
+    return default(LANGUAGES, 'description', **kwargs)
+
+
+def project_creator(**kwargs) -> Dict[str, str]:
+    """
+    :param kwargs:
+    :return:
+    """
+    from_creator = CustomUser.objects.get(id=kwargs['from'])
+    to_creator = CustomUser.objects.get(id=kwargs['to'])
+    context = {'from': from_creator.username, 'to': to_creator}
+    return default(LANGUAGES, 'creator', **context)
 
 
 PROJECT_FIELDS = {
     'Project.title': project_title,
+    'Project.short': project_short,
+    'Project.creator': project_creator,
+    'Project.description': project_description,
 }
--- core/projects/localization/en/projects.py
+++ core/projects/localization/en/projects.py
@@ -1,3 +1,6 @@
 FIELDS_EN = {
-    'title': lambda **kwargs: ' renamed project "{from}" to "{to}"'.format(**kwargs),
+    'title': lambda **kwargs: ' renamed project from "{from}" to "{to}"'.format(**kwargs),
+    'short': lambda **kwargs: ' changed short name of project from "{from}" to "{to}"'.format(**kwargs),
+    'description': lambda **kwargs: ' changed description of project from "{from}" to "{to}"'.format(**kwargs),
+    'creator': lambda **kwargs: ' changed creator of project from "@{from}" to "@{to}"'.format(**kwargs),
 }
