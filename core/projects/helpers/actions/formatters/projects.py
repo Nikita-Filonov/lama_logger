a66f3732f4621d3b3bf342907efe873148c4acdb
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2021-11-20 01:39:29 +0300

    Трэки каналов

--- core/projects/helpers/actions/formatters/projects.py
+++ core/projects/helpers/actions/formatters/projects.py
@@ -10,38 +10,30 @@ LANGUAGES = {
 
 
 def project_title(**kwargs) -> Dict[str, str]:
-    """
-    :param kwargs:
-    :return:
-    """
     return default(LANGUAGES, 'title', **kwargs)
 
 
 def project_short(**kwargs) -> Dict[str, str]:
-    """
-    :param kwargs:
-    :return:
-    """
     return default(LANGUAGES, 'short', **kwargs)
 
 
 def project_description(**kwargs) -> Dict[str, str]:
-    """
-    :param kwargs:
-    :return:
-    """
     return default(LANGUAGES, 'description', **kwargs)
 
 
 def project_creator(**kwargs) -> Dict[str, str]:
-    """
-    :param kwargs:
-    :return:
-    """
     from_creator = CustomUser.objects.get(id=kwargs['from'])
     to_creator = CustomUser.objects.get(id=kwargs['to'])
     context = {'from': from_creator.username, 'to': to_creator}
     return default(LANGUAGES, 'creator', **context)
+
+
+def project_telegram_channel(**kwargs) -> Dict[str, str]:
+    return default(LANGUAGES, 'telegramChannel', **kwargs)
+
+
+def project_slack_channel(**kwargs) -> Dict[str, str]:
+    return default(LANGUAGES, 'slackChannel', **kwargs)
 
 
 PROJECT_FIELDS = {
@@ -49,4 +41,6 @@ PROJECT_FIELDS = {
     'Project.short': project_short,
     'Project.creator': project_creator,
     'Project.description': project_description,
+    'Project.slackChannel': project_slack_channel,
+    'Project.telegramChannel': project_telegram_channel,
 }
--- core/projects/localization/en/projects.py
+++ core/projects/localization/en/projects.py
@@ -3,4 +3,6 @@ FIELDS_EN = {
     'short': lambda **kwargs: ' changed short name of project from "{from}" to "{to}"'.format(**kwargs),
     'description': lambda **kwargs: ' changed description of project from "{from}" to "{to}"'.format(**kwargs),
     'creator': lambda **kwargs: ' changed creator of project from "@{from}" to "@{to}"'.format(**kwargs),
+    'telegramChannel': lambda **kwargs: ' changed Telegram channel setting from "{from}" to "{to}"'.format(**kwargs),
+    'slackChannel': lambda **kwargs: ' changed Slack channel setting from "{from}" to "{to}"'.format(**kwargs),
 }
