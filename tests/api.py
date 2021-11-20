3acc4279a99003e1c680445f90b91d7a57f08d22
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2021-11-20 23:25:56 +0300

    Автотесты для проектов

--- tests/api.py
+++ tests/api.py
@@ -11,9 +11,27 @@ class Endpoints(Enum):
     PROJECTS = '/api/v1/projects/'
 
 
+def get(url, user=None, with_auth=True, **kwargs):
+    with allure.step(f'Marking GET request to {url}'):
+        request = factory.get(url, **kwargs)
+        if with_auth and user is not None:
+            force_authenticate(request, user=user)
+
+        return request
+
+
 def post(url, data, user=None, with_auth=True, **kwargs):
     with allure.step(f'Marking POST request to {url} with payload {data}'):
         request = factory.post(url, data=json.dumps(data), content_type='application/json', **kwargs)
+        if with_auth and user is not None:
+            force_authenticate(request, user=user)
+
+        return request
+
+
+def patch(url, data, user=None, with_auth=True, **kwargs):
+    with allure.step(f'Marking PATCH request to {url} with payload {data}'):
+        request = factory.patch(url, data=json.dumps(data), content_type='application/json', **kwargs)
         if with_auth and user is not None:
             force_authenticate(request, user=user)
 
--- tests/projects/test_projects_api.py
+++ tests/projects/test_projects_api.py
@@ -1,10 +1,10 @@
 import allure
 import pytest
-from rest_framework.status import HTTP_201_CREATED
+from rest_framework import status
 
 from core.projects.permissions.roles import DEFAULT_ROLES
 from core.projects.views.projects import ProjectsApi, ProjectApi
-from tests.api import Endpoints, post
+from tests.api import Endpoints, post, get, patch
 from tests.params import get_project_payload
 from tests.utils.utils import to_json
 
@@ -20,6 +20,14 @@ projects_api = Endpoints.PROJECTS.value
 @allure.severity(allure.severity_level.CRITICAL)
 @pytest.mark.django_db(transaction=True)
 class TestMonstersApi:
+
+    @allure.title('Get projects')
+    def test_get_projects(self, user):
+        request = get(projects_api, user)
+        response = projects_view(request)
+
+        assert response.status_code == status.HTTP_200_OK
+
     @allure.title('Create project')
     def test_create_project(self, user):
         project_payload = get_project_payload()
@@ -28,9 +36,38 @@ class TestMonstersApi:
         response = projects_view(request)
         json_response = to_json(response)
 
-        assert response.status_code == HTTP_201_CREATED
+        assert response.status_code == status.HTTP_201_CREATED
         assert json_response['title'] == project_payload['title']
         assert json_response['short'] == project_payload['short']
         assert json_response['description'] == project_payload['description']
         assert len(json_response['members']) == 1
         assert len(json_response['roles']) == len(DEFAULT_ROLES)
+
+    @allure.title('Update project')
+    def test_update_project(self, project, user):
+        project_payload = get_project_payload()
+        request = patch(projects_api + f'{project["id"]}/', project_payload, user)
+
+        response = project_view(request, project_id=project["id"])
+        json_response = to_json(response)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert json_response['title'] == project_payload['title']
+        assert json_response['short'] == project_payload['short']
+        assert json_response['description'] == project_payload['description']
+        assert len(json_response['members']) == 1
+        assert len(json_response['roles']) == len(DEFAULT_ROLES)
+
+    @allure.title('Get project')
+    def test_get_project(self, project, user):
+        request = get(projects_api + f'{project["id"]}/', user)
+        response = project_view(request, project_id=project["id"])
+        json_response = to_json(response)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert json_response['id'] == project['id']
+        assert json_response['title'] == project['title']
+        assert json_response['short'] == project['short']
+        assert json_response['description'] == project['description']
+        assert json_response['members'] == project['members']
+        assert json_response['roles'] == project['roles']
